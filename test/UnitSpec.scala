7a00144edf22fe65f0e043c22ea196168668f529
Stefano S <kindoblue@gmail.com>
2015-04-25 15:33:02 +0200

    Added Parser test

--- test/UnitSpec.scala
+++ test/UnitSpec.scala
@@ -5,20 +5,18 @@ import org.specs2.mutable._
 import core._
 import validation._
 
-import scala.util.{Failure, Success}
-
 class UnitSpec extends Specification {
-
-  val certs = TestDataGenerator.generateTestCertificates("CN=Stefano", "CN=Intermediate", "CN=End cert")
-
-  val (root : X509Certificate, intermediate: X509Certificate, endCert: X509Certificate, privateKey: PrivateKey) = certs
-
-  val envelope = TestDataGenerator.createSignedData("This is a test", root, intermediate, endCert, privateKey)
-
-  val wrong = TestDataGenerator.generateSingleCertificate("CN=Wrong")
 
 
   "The Validator" should {
+
+    val certs = TestDataGenerator.generateTestCertificates("CN=Stefano", "CN=Intermediate", "CN=End cert")
+
+    val (root : X509Certificate, intermediate: X509Certificate, endCert: X509Certificate, privateKey: PrivateKey) = certs
+
+    val envelope = TestDataGenerator.createSignedData("This is a test", root, intermediate, endCert, privateKey)
+
+    val wrong = TestDataGenerator.generateSingleCertificate("CN=Wrong")
 
     "validate successfully the envelope when using the right anchor" in {
 
@@ -32,6 +30,17 @@ class UnitSpec extends Specification {
 
     }
 
-
   }
+
+  "The Parser" should {
+
+    val receiptURL = getClass.getResource("/1000000101882225.cer")
+
+    "parse correctly a valid receipt" in {
+
+      Parser.parsePurchasesFromURL(receiptURL) must beSuccessfulTry
+
+    }
+  }
+
 }
