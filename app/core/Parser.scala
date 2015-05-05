bb22137c278874cd74e46709bb5c3ac8f9dc975b
Stefano S <kindoblue@gmail.com>
2015-05-05 15:29:35 +0200

    Better documentation and refactoring

--- app/core/Parser.scala
+++ app/core/Parser.scala
@@ -28,21 +28,43 @@ import scala.collection.JavaConverters._
 
 object Parser {
 
-  private def parsePurchaseField(field: Any) = {
-    val fieldType = field.asInstanceOf[DLSequence].getObjectAt(0) match {
+  /**
+   * It gets a DLSequence representing a field of the purchase record,
+   * then parse and return it as a Map
+   *
+   * The DLSequence is something like:
+   *
+   *    [1704, 1, #1614323031342d30322d31395431333a32363a35345a]
+   *
+   * where the first element is the field type, the second is the version and
+   * the third is the asn1 encoded value
+   *
+   * @param field the DLSequence representing a purchase field
+   * @return a Map representing the parsed input field
+   */
+  private def parsePurchaseField(field: DLSequence) = {
+
+    // get the field type as a plain integer
+    val fieldType = field.getObjectAt(0) match {
       case zz: ASN1Integer => zz.getValue.intValue
       case _ => throw new ClassCastException("Expected an integer as field type")
     }
-    val fieldValue = field.asInstanceOf[DLSequence].getObjectAt(2) match {
+
+    // construct an ASN1primitive from the third element of the input sequence
+    val fieldValue = field.getObjectAt(2) match {
       case z2: ASN1OctetString => ASN1Primitive.fromByteArray(z2.getOctets)
       case _ => throw new ClassCastException("Expected an ASN1OctetString as field value")
     }
+
+    // interpret the field value
     val f = fieldValue match {
       case s: DERUTF8String => s.getString
       case z: ASN1Integer => z.getValue.intValue
       case d: DERIA5String =>  d.getString
       case _ =>   throw new ClassCastException("Field value not expected")
     }
+
+    // construct the Map according the field type
     fieldType match {
       case 1701 => Map("quantity" -> f)
       case 1702 => Map("product-id" -> f)
@@ -56,18 +78,29 @@ object Parser {
     }
   }
 
-  private def parsePurchase(purchase: Any)  = {
-    val dlSeq = purchase match {
-      case z: DLSequence => z.getObjectAt(2)
-      case _ => throw new ClassCastException("Expected a DLSequence as a purchase record")
-    }
-    val fieldSet = dlSeq match  {
-      case z2: ASN1OctetString => ASN1Primitive.fromByteArray(z2.getOctets)
+  /**
+   * It gets a DLSequence representing a purchase and parses it.
+   *
+   * The DLSequence is something like this:
+   *
+   *      [17, 1, #3182014b300b020206.....]
+   *
+   * where the first element is the field type (always 17 for purchase record),
+   * the second is the version and the third is the asn1 encoded set (of purchase fields)
+   *
+   * @param purchase the DLSequence representing the purchase
+   * @return a Map containing all the fields of the input purchase record
+   */
+  private def parsePurchase(purchase: DLSequence) : Map[_ <: String, Any]  = {
+
+    // get the asn1 representation of the purchase record
+    val fieldSet : ASN1Set = purchase.getObjectAt(2) match  {
+      case z2: ASN1OctetString => ASN1Primitive.fromByteArray(z2.getOctets).asInstanceOf[ASN1Set]
       case _ => throw new ClassCastException
     }
 
-    // get the iterator on fields
-    val fieldIterator : Iterator[Any] = fieldSet.asInstanceOf[ASN1Set].getObjects.asScala
+    // get the iterator on the fields of the set
+    val fieldIterator : Iterator[DLSequence] = fieldSet.getObjects.asInstanceOf[java.util.Enumeration[DLSequence]].asScala
 
     // reduce to a single map with a key for every field
     fieldIterator.map(parsePurchaseField).reduce(_++_)
@@ -75,30 +108,37 @@ object Parser {
   }
 
   /**
-   * It takes an object and verifies if it is a purchase record
+   * It gets a DLSequence representing a receipt record and check if it is purchase record.
+   *
+   * The DLSequence is something like this:
+   *
+   *      [17, 1, #3182014b300b020206.....]
+   *
+   * where the first element is the field type (always 17 for purchase record),
+   * the second is the version and the third is the asn1 encoded set (of purchase fields)
    *
    * @param maybePurchase
    * @return true if the input is a purchase record
    */
-  private def isPurchase(maybePurchase: Any) : Boolean = {
-    val dlSeq = maybePurchase match {
-      case z: DLSequence => z
-      case _ => throw new ClassCastException("Expected a DLSequence as a purchase record")
-    }
-    val a = dlSeq.getObjectAt(0)  match {
+  private def isPurchase(maybePurchase: DLSequence) : Boolean = {
+
+    // get the record type as a simple integer
+    val recordType = maybePurchase.getObjectAt(0)  match {
       case purchaseTag: ASN1Integer => purchaseTag.getValue.intValue
-      case _ => throw new ClassCastException("Expected an integer as field id in receipt")
+      case _ => throw new ClassCastException("Expected an integer as field id in receipt record")
     }
 
-    val b = dlSeq.getObjectAt(1)  match {
+    // for purchase record, the record type is 17
+    recordType  == 17
 
-      case zz: ASN1Integer => zz.getValue.intValue
-      case _ => throw new ClassCastException("Expected an integer as field id in receipt")
-    }
-
-    a  == 17 && b == 1
   }
 
+  /**
+   * Get the signed data object from the receipt
+   *
+   * @param stream the stream from where to read the signed data
+   * @return the signed data object
+   */
   private def getSignedData(stream: InputStream): CMSSignedData = {
 
     // create an asn1 stream
@@ -112,7 +152,14 @@ object Parser {
     new CMSSignedData(contentInfo)
   }
 
-  private def getContentIterator(signedData: CMSSignedData) : Iterator[Any] = {
+  /**
+   * The signed data of a in-app purchase would be an asn1 encoded list of DLSequence's.
+   * This method return an Iterator on the set of DLSequence's of the receipt
+   *
+   * @param signedData the signed data from the receipt
+   * @return the iterator on the set of DLSequences of the receipt
+   */
+  private def getContentIterator(signedData: CMSSignedData) : Iterator[DLSequence] = {
 
     // retrieve the byte array of the signed content and create a
     // ASN1Primitive generic object out of it
@@ -123,7 +170,7 @@ object Parser {
 
     // if the asn1 set is correctly retrieved, we return the scala iterator
     asn1Set match {
-      case ff2: ASN1Set => ff2.getObjects.asScala
+      case ff2: ASN1Set => ff2.getObjects.asInstanceOf[java.util.Enumeration[DLSequence]].asScala
       case _ => throw new ClassCastException("Expected an ASN1Set object as content in signed data")
     }
   }
